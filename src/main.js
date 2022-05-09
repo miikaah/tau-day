8632f4e005f188bcb06f7aa198a4dcf64537c866
Miika Henttonen <miika.henttonen@wunderdog.fi>
2022-05-09 10:13:40 +0300

    Code improvements

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ const height = width;
 const lw = 1;
 const padding = 50;
 const r = width / 2 - padding - lw;
-const o = r + padding + lw;
-const a = 0 + 40;
+const o = width / 2;
+const a = 40;
 const b = 2 * r + padding + lw + 10;
 
 const tau = Math.PI * 2;
-const cos = Math.cos;
-const sin = Math.sin;
+const cos = (fraction) => Math.cos(rad(fraction));
+const sin = (fraction) => Math.sin(rad(fraction));
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -44,28 +44,28 @@ const r2 = r + 20;
 ctx.strokeStyle = "#000";
 ctx.textBaseline = "middle";
 ctx.fillText("0 t", o + r2, o);
-ctx.fillText("1 / 8 t", o + r2 * cos(rad(1 / 8)), o - r2 * sin(rad(1 / 8)));
-ctx.fillText("2 / 8 t", o - 14 + r2 * cos(rad(1 / 4)), o - r2 * sin(rad(1 / 4)));
-ctx.fillText("3 / 8 t", o - 30 + r2 * cos(rad(3 / 8)), o - r2 * sin(rad(3 / 8)));
-ctx.fillText("4 / 8 t", o - 30 + r2 * cos(rad(1 / 2)), o);
-ctx.fillText("5 / 8 t", o - 30 + r2 * cos(rad(5 / 8)), o - r2 * sin(rad(5 / 8)));
-ctx.fillText("6 / 8 t", o - 14, o + 4 - r2 * sin(rad(3 / 4)));
-ctx.fillText("7 / 8 t", o - r2 * cos(rad(5 / 8)), o - r2 * sin(rad(5 / 8)));
+ctx.fillText("1 / 8 t", o + r2 * cos(1 / 8), o - r2 * sin(1 / 8));
+ctx.fillText("2 / 8 t", o - 14 + r2 * cos(1 / 4), o - r2 * sin(1 / 4));
+ctx.fillText("3 / 8 t", o - 30 + r2 * cos(3 / 8), o - r2 * sin(3 / 8));
+ctx.fillText("4 / 8 t", o - 30 + r2 * cos(1 / 2), o);
+ctx.fillText("5 / 8 t", o - 30 + r2 * cos(5 / 8), o - r2 * sin(5 / 8));
+ctx.fillText("6 / 8 t", o - 14, o + 4 - r2 * sin(3 / 4));
+ctx.fillText("7 / 8 t", o - r2 * cos(5 / 8), o - r2 * sin(5 / 8));
 
 // Draw triangle
 ctx.strokeStyle = "#999";
 ctx.beginPath();
-ctx.moveTo(o + r * cos(rad(1 / 8)), o - 20);
-ctx.lineTo(o + r * cos(rad(1 / 8)) - 20, o - 20);
-ctx.lineTo(o + r * cos(rad(1 / 8)) - 20, o);
+ctx.moveTo(o + r * cos(1 / 8), o - 20);
+ctx.lineTo(o + r * cos(1 / 8) - 20, o - 20);
+ctx.lineTo(o + r * cos(1 / 8) - 20, o);
 ctx.arc(o, o, 20, 0, rad(-1 / 8), true);
 ctx.stroke();
 
 ctx.strokeStyle = "#000";
 ctx.beginPath();
 ctx.moveTo(o, o);
-ctx.lineTo(o + r * cos(rad(1 / 8)), o - r * sin(rad(1 / 8)));
-ctx.lineTo(o + r * cos(rad(1 / 8)), o);
+ctx.lineTo(o + r * cos(1 / 8), o - r * sin(1 / 8));
+ctx.lineTo(o + r * cos(1 / 8), o);
 ctx.lineTo(o, o);
 ctx.stroke();
 
